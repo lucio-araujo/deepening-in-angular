dcea2b97c8772c2ca38d4a32ef9171987600be95
lucio-araujo <lucio.ribeiroaraujo@outlook.com>
2021-12-10 15:35:16 -0300

    feat(dep): adição materialize

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaterializeModule } from './materialize.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MaterializeModule
+  ]
+})
+export class SharedModule { }

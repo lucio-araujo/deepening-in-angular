9d214cb2d7d831d74293bbdb5f6dbfd81d8a0544
lucio-araujo <lucio.ribeiroaraujo@outlook.com>
2021-12-11 09:45:20 -0300

    feat(component): implementação final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppComponent } from './app.component';
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { ReactiveFormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { RouterModule, Routes } from "@angular/router";
 
-import { SharedModule } from './shared/shared.module';
-import { ConfiguracaoComponent } from './components/configuracao/configuracao.component';
-import { MaterialModule } from './shared/material.module';
-import { ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from "./app.component";
+import { ConfiguracaoComponent } from "./components/configuracao/configuracao.component";
+
+import { MaterialModule } from "./shared/material.module";
+import { SharedModule } from "./shared/shared.module";
+import { FlexLayoutModule } from "@angular/flex-layout";
+
+const routes: Routes = [
+  { path: "configuracao", component: ConfiguracaoComponent },
+  { path: "**", redirectTo: "configuracao" },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ConfiguracaoComponent
-  ],
+  declarations: [AppComponent, ConfiguracaoComponent],
   imports: [
     SharedModule,
     MaterialModule,
+    FlexLayoutModule,
     ReactiveFormsModule,
-    AppRoutingModule,
+    HttpClientModule,
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

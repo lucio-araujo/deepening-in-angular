736ae10bf2b9be16e98fc77e064017d26b6e4c19
lucio-araujo <lucio.ribeiroaraujo@outlook.com>
2021-12-11 07:55:09 -0300

    feat: criação material.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ConfiguracaoComponent } from './components/configuracao/configuracao.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+
+import { SharedModule } from './shared/shared.module';
+import { ConfiguracaoComponent } from './components/configuracao/configuracao.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     ConfiguracaoComponent
   ],
   imports: [
+    SharedModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    MaterialModule,
   ]
 })
 export class SharedModule { }
